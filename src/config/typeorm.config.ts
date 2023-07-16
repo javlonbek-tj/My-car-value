fc38b7760b58d1ab127920f0972518688ba97f66
tjdeveloper <tjmr1996@gmail.com>
2023-07-16 19:53:17 +0500

    migration added

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,13 +7,19 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    const isTestEnvironment = process.env.NODE_ENV === 'test';
+    const isDevEnvironment = process.env.NODE_ENV === 'development';
+    const isProdEnvironment = process.env.NODE_ENV === 'production';
+
     return {
-      type: 'sqlite',
-      synchronize: process.env.NODE_ENV === 'test' ? true : false,
-      database: this.configService.get<string>('DB_NAME'),
+      type: 'sqlite', // Change this to 'postgres' or your desired production database type
+      synchronize: isTestEnvironment || isDevEnvironment, // Synchronize in test and development, but not in production
+      database: this.configService.get<string>('DATABASE_NAME'),
       autoLoadEntities: true,
-      migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
-      keepConnectionAlive: process.env.NODE_ENV === 'test' ? true : false,
+      migrationsRun: isProdEnvironment, // Set to true in production mode
+      keepConnectionAlive: isTestEnvironment,
+      entities: ['**/*.entity.ts'],
+      migrations: [__dirname + '/migrations/*.ts'],
     };
   }
 }
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,27 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-export const appDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'db.sqlite',
-  entities: ['**/*.entity.ts'],
-  migrations: [__dirname + '/migrations/*.ts'],
-} as DataSourceOptions);
+const isTestEnvironment = process.env.NODE_ENV === 'test';
+const isProdEnvironment = process.env.NODE_ENV === 'production';
+
+const dataSourceOptions: DataSourceOptions = isTestEnvironment
+  ? {
+      type: 'sqlite',
+      database: 'test.sqlite',
+    }
+  : isProdEnvironment
+  ? {
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: ['**/*.entity.ts'],
+      migrations: [__dirname + '/migrations/*.ts'],
+    }
+  : {
+      type: 'sqlite',
+      database: 'db.sqlite',
+    };
+
+export const appDataSource = new DataSource(dataSourceOptions);
